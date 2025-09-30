dfcbee9de85bae3a69196dec5f65127f10d0fe4c
victorolivo24 <victormolivo3@gmail.com>
2025-09-29 21:00:47 -0400

    initialize chess folder iwth all default java files

---
+++ chess/Chess.java
@@ -0,0 +1,34 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class Chess {
+
+        enum Player { white, black }
+    
+	/**
+	 * Plays the next move for whichever player has the turn.
+	 * 
+	 * @param move String for next move, e.g. "a2 a3"
+	 * 
+	 * @return A ReturnPlay instance that contains the result of the move.
+	 *         See the section "The Chess class" in the assignment description for details of
+	 *         the contents of the returned ReturnPlay instance.
+	 */
+	public static ReturnPlay play(String move) {
+
+		/* FILL IN THIS METHOD */
+		
+		/* FOLLOWING LINE IS A PLACEHOLDER TO MAKE COMPILER HAPPY */
+		/* WHEN YOU FILL IN THIS METHOD, YOU NEED TO RETURN A ReturnPlay OBJECT */
+		return null;
+	}
+	
+	
+	/**
+	 * This method should reset the game, and start from scratch.
+	 */
+	public static void start() {
+		/* FILL IN THIS METHOD */
+	}
+}
