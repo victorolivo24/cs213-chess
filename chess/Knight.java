4b9f39b0eb6bf72bcb4ec28b3ea851056e564aa5
melanirod15 <melanirod15@hotmail.com>
2025-10-06 01:36:34 -0400

    knight: added knight L-shaped movement

--- chess/Knight.java
+++ chess/Knight.java
@@ -1,5 +1,20 @@
 package chess;
 
-public class Knight {
+public class Knight extends Piece {
+    public Knight (Color color)
+    {
+        super(color, PieceType.KNIGHT);
+    }
     
+
+    public boolean isValidMove (Board board, Square from, Square to)
+    {
+        if (from.equals(to)) return false;
+
+        int df = Math.abs(to.file() - from.file());
+        int dr = Math.abs(to.rank() - from.rank());
+
+        return (df == 2 && dr == 1) || (df == 1 && dr == 2);
+        
+    }
 }
