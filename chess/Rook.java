50529077009e3570c204e07d47237c3f2f75fe7d
melanirod15 <melanirod15@hotmail.com>
2025-10-06 01:24:23 -0400

    rook: added straight-line move geometry

--- chess/Rook.java
+++ chess/Rook.java
@@ -1,5 +1,25 @@
 package chess;
 
-public class Rook {
+public class Rook extends Piece{
     
+    public Rook (Color color)
+    {
+        super(color, PieceType.ROOK);
+    }
+
+    public boolean isValidMove(Board board, Square from, Square to)
+    {
+        //can't stay in place
+        if (from.equals(to)) return false;
+
+        //differences along file (x) and rank (y)
+        int df= to.file() - from. file(); //horizontal
+        int dr = to.rank() - from.rank(); // vertical
+
+        //rook moves in straight lines only:
+        boolean vertical = (df == 0) && (dr !=0);
+        boolean horizontal = (dr == 0) && (df != 0);
+
+        return vertical || horizontal;
+    }
 }
