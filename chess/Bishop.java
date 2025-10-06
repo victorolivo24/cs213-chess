07e7863d0dc3a52d7226d55497e64d1d80e1a830
melanirod15 <melanirod15@hotmail.com>
2025-10-06 01:43:35 -0400

    bishop: added diagonal move geometry

--- chess/Bishop.java
+++ chess/Bishop.java
@@ -1,5 +1,20 @@
 package chess;
 
-public class Bishop {
+public class Bishop extends Piece {
+    public Bishop (Color color)
+    {
+        super(color, PieceType.BISHOP);
+    }
     
+
+    public boolean isValidMove (Board board, Square from, Square to)
+    {
+        if (from.equals(to)) return false;
+
+        int df = Math.abs(to.file() - from.file());
+        int dr = Math.abs(to.rank() - from.rank());
+
+        return df == dr && df != 0;
+        
+    }
 }
